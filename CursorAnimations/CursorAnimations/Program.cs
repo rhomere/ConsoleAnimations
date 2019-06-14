5e0333dc2f05e9e3e146dcce1acb607f0e4ac8ca
Renel Homere <rhomere@gmail.com>
2019-06-13 23:03:14 -0400

    add draw gif

--- CursorAnimations/CursorAnimations/Program.cs
+++ CursorAnimations/CursorAnimations/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,8 +16,48 @@ namespace CursorAnimations
             // Spinning
             //SpinningCursor();
 
-            StarWarsIntro();
+            //StarWarsIntro();
+
+            DrawGif();
             
+        }
+
+        private static void DrawGif()
+        {
+            Image image = Image.FromFile(@"C:\projects\some_animated_gif.gif");
+            FrameDimension dimension = new FrameDimension(
+                               image.FrameDimensionsList[0]);
+            int frameCount = image.GetFrameCount(dimension);
+            StringBuilder sb;
+
+            // Remember cursor position
+            int left = Console.WindowLeft, top = Console.WindowTop;
+
+            char[] chars = { '#', '#', '@', '%', '=', '+',
+                         '*', ':', '-', '.', ' ' };
+            for (int i = 0; ; i = (i + 1) % frameCount)
+            {
+                sb = new StringBuilder();
+                image.SelectActiveFrame(dimension, i);
+
+                for (int h = 0; h < image.Height; h++)
+                {
+                    for (int w = 0; w < image.Width; w++)
+                    {
+                        Color cl = ((Bitmap)image).GetPixel(w, h);
+                        int gray = (cl.R + cl.G + cl.B) / 3;
+                        int index = (gray * (chars.Length - 1)) / 255;
+
+                        sb.Append(chars[index]);
+                    }
+                    sb.Append('\n');
+                }
+
+                Console.SetCursorPosition(left, top);
+                Console.Write(sb.ToString());
+
+                System.Threading.Thread.Sleep(100);
+            }
         }
 
         private static void StarWarsIntro()
