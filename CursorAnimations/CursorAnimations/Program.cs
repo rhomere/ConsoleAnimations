c7ef1ba322c3bb66f808063232ec23ddf655aebd
Renel Homere <rhomere@gmail.com>
2019-06-13 22:13:45 -0400

    add ConsoleSpinner.cs

--- CursorAnimations/CursorAnimations/Program.cs
+++ CursorAnimations/CursorAnimations/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CursorAnimations
@@ -10,6 +11,28 @@ namespace CursorAnimations
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            Console.WriteLine("Press CTRL-C to exit.");
+
+            var s = new ConsoleSpinner();
+
+            while (true)
+            {
+                Thread.Sleep(100); // simulate some work being done
+                s.UpdateProgress();
+            }
+        }
+
+        public void UpdateProgress()
+        {
+            // Hide cursor to improve the visuals (note: we should re-enable at some point)
+            Console.CursorVisible = false;
+
+            // Store the current position of the cursor
+            var originalX = Console.CursorLeft;
+            var originalY = Console.CursorTop;
+
+            // etc.
         }
     }
 }
